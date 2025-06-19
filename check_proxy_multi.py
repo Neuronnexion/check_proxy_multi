7e9f9aa6784569e2f668092f41fadad631b506d3
Dominique Rousseau <d.rousseau@nnx.com>
2025-06-19 16:15:31 +0200

    wip, use arg for proxy ip and port, somme error checks

--- check_proxy_multi.py
+++ check_proxy_multi.py
@@ -84,21 +84,26 @@ def load_urls(filename):
 
     return urls
 
-def help(exitc=0):
-    print ('check_proxy_multi.py [-h] [-v] -f <urls_file> [-n <number_of_checks>] [-t <timeout>]')
+def help(exitc=0, msg=None):
+    # -I 10.5.8.13 -p 3131
+    if ( msg is not None ):
+        print (msg)
+    print ('check_proxy_multi.py [-h] [-v] -I <proxy_ip> -p <proxy_port> -f <urls_file> [-n <number_of_checks>] [-t <timeout>]')
     sys.exit(exitc)
 
 def main(argv):
     global VERBOSE
 
     try:
-        opts, args = getopt.getopt(argv,"hvf:n:t:",["urlsfile=","nchecks=","timeout="])
+        opts, args = getopt.getopt(argv,"hvI:p:f:n:t:",["urlsfile=","nchecks=","timeout="])
     except getopt.GetoptError:
         help(2)
 
     urlsfile = None
     nchecks = 3
     timeout = 5
+    proxy_ip = None
+    proxy_port = None
 
     for opt, arg in opts:
         #print (opt, arg)
@@ -106,6 +111,10 @@ def main(argv):
             help()
         elif opt == '-v':
             VERBOSE = True
+        elif (opt == '-I'):
+            proxy_ip = arg
+        elif (opt == '-p'):
+            proxy_port = arg
         elif (opt == '-f') or (opt == '--urlsfile'):
             #print (arg)
             urlsfile = arg
@@ -117,13 +126,29 @@ def main(argv):
             timeout = int(arg)
 
     if ( urlsfile is None ):
-        help(-1)
+        help(-1, "Missing file with list of urls to check")
+
+    if ( proxy_ip is None ):
+        help(-1, "Missing IP of proxy host")
+
+    if ( proxy_port is None ):
+        help(-1, "Missing port of proxy host")
 
     #connection_mgr = urllib3.PoolManager(maxsize=5)
-    connection_mgr = urllib3.ProxyManager('http://10.5.1.86:3131/', maxsize=5)
-    thread_pool = ThreadPoolExecutor(5)
+    proxy_url = 'http://%s:%s/' % ( proxy_ip, proxy_port )
 
-    urls = load_urls(urlsfile)
+    #connection_mgr = urllib3.ProxyManager('http://10.5.1.86:3131/', maxsize=5)
+    try:
+        connection_mgr = urllib3.ProxyManager(proxy_url, maxsize=5)
+        thread_pool = ThreadPoolExecutor(5)
+    except Exception as e:
+        print(e)
+        exitcode ('U', 'Can not use proxy at %s' % proxy_url)
+
+    try:
+        urls = load_urls(urlsfile)
+    except Exception as e:
+        exitcode ('U', 'Can not open file %s' % urlsfile)
 
     # max is the number of url in list
     if ( nchecks > len(urls) ):
